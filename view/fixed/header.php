dc1a06de813f27877d2c7ea573d32c2f48ad8db4
marijanamilanovic <95504389+marijanamilanovic@users.noreply.github.com>
2024-12-18 15:15:12 +0100

    Add files via upload

---
+++ view/fixed/header.php
@@ -0,0 +1,23 @@
+<header id="site-header">
+	<div class="container">
+		<nav class="navbar navbar-expand-lg navbar-dark stroke p-0">
+			<a class="navbar-brand" href="index.php">
+				<img src="assets/images/favicon.ico" alt="logo" style="height:40px;" />
+			</a> 
+			<h1>
+				<a class="navbar-brand" href="index.php">Foodster</a>
+			</h1>
+			<button class="navbar-toggler  collapsed bg-gradient" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
+				aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon fa icon-expand fa-bars"></span>
+				<span class="navbar-toggler-icon fa icon-close fa-times"></span>
+				</span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+				<ul id="navigation" class="navbar-nav mx-auto">
+
+				</ul>
+			</div>
+		</nav>
+	</div>
+</header>
